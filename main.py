c2de8cfb5a36dcfacfced5c84ccc32d5d70b0366
Deven <deven9852@gmail.com>
2017-10-20 11:47:19 +0530

    Computer performs at optimal levels.

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ mainBoard = Board()
 mainComputer = Computer(mainBoard)
 
 # Computer saves data for n random games
-mainComputer.playGames(100000)
+mainComputer.playGames(400000)
 
 # Start Human-Computer game here
 
-while ((mainBoard.isWin() is not 1)):
+while (mainBoard.isWin() is not 1) and (mainBoard.movesPlayed is not 9) and (mainBoard.isWin() is not 2):
 
     print("Move " + str(mainBoard.movesPlayed))
 
