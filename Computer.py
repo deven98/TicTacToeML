88d3b2ced716d6364473aba50c0054c9d92e4d66
Deven <deven9852@gmail.com>
2017-10-20 10:24:15 +0530

    Tested first few moves.

--- Computer.py
+++ Computer.py
@@ -1,5 +1,7 @@
 # Class that runs all games and stores them into a file/list
 from random import randint
+import itertools
+import operator
 
 class Computer(object):
 
@@ -21,13 +23,31 @@ class Computer(object):
         while self.board.isWin() is 0:
             self.board.makeMove(randint(0,8))
 
-        # Triggers if someone wins. "Win" here is player 1 winning and "Loss" implies a player 2 win
+        # Triggers if someone wins. "Win" here is player 1 winning and "Loss" implies a player 2 win.
         if self.board.isWin() is 1:
             if self.board.playerToMove is 2:
                 self.winList.append(self.board.moveList[:])
             elif self.board.playerToMove is 1:
                 self.lossList.append(self.board.moveList[:])
 
-        print(self.board.moveList)
-
         self.board.resetBoard()
+
+    def most_common(self,L):
+        # get an iterable of (item, iterable) pairs
+        SL = sorted((x, i) for i, x in enumerate(L))
+        # print 'SL:', SL
+        groups = itertools.groupby(SL, key=operator.itemgetter(0))
+
+        # auxiliary function to get "quality" for an item
+        def _auxfun(g):
+            item, iterable = g
+            count = 0
+            min_index = len(L)
+            for _, where in iterable:
+                count += 1
+                min_index = min(min_index, where)
+            # print 'item %r, count %r, minind %r' % (item, count, min_index)
+            return count, -min_index
+
+        # pick the highest-count/earliest item
+        return max(groups, key=_auxfun)[0]
--- main.py
+++ main.py
@@ -4,7 +4,39 @@ from Computer import Computer
 mainBoard = Board()
 mainComputer = Computer(mainBoard)
 
-mainComputer.playGames(10)
+# Computer saves data for n random games
+mainComputer.playGames(500000)
 
-print(mainComputer.winList)
-print(mainComputer.lossList)
+# Creates a list of first moves
+commonList = []
+for game in mainComputer.winList:
+    commonList.append(game[0])
+
+# Prints recommended first move
+print(mainComputer.most_common(commonList))
+
+secondMoveList = []
+
+for game in mainComputer.winList:
+    if game[0] is 4 and game[1] is 3:
+        secondMoveList.append(game)
+
+commonList.clear()
+
+for game in secondMoveList:
+    commonList.append(game[2])
+
+print(mainComputer.most_common(commonList))
+
+thirdMoveList = []
+
+for game in mainComputer.winList:
+    if game[0] is 4 and game[1] is 3 and game[2] is 8 and game[3] is 6:
+        thirdMoveList.append(game)
+
+commonList.clear()
+
+for game in thirdMoveList:
+    commonList.append(game[4])
+
+print(mainComputer.most_common(commonList))
